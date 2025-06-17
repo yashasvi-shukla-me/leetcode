d2167eefddbe7d6584e130885a3bf6400b8aa86d
Yashasvi Shukla <yashasvigot@gmail.com>
2025-06-17 18:38:12 +0530

    Two Sum II solution

---
+++ array/167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,70 @@
+"""
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+"""
+
+#   Brute Force
+#   Time limit exceeded
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        for i in range(len(numbers)):
+            for j in range(i+1, len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return [i+1,j+1]
+
+
+#   Using Two Pointers (Best Approach)
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            curr_sum = numbers[left] + numbers[right]
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            else:
+                if numbers[left] + numbers[right] < target:
+                    left = left + 1
+                else:
+                    right = right - 1
+
+
+#   Using Dictionary (Hash Map)
+#   Not uses constant time
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        dictionary = {}
+
+        for i, num in enumerate(numbers):
+            complement = target - num
+            if complement in dictionary:
+                return [dictionary[complement] + 1, i + 1]
+            dictionary[num] = i
