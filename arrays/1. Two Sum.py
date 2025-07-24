36a9cf2bf496829b771fd79dfd8bf68d5ab52c96
Yashasvi Shukla <yashasvigot@gmail.com>
2025-07-24 14:07:41 +0530

    updated SOLUTION for Two Sum

--- arrays/1. Two Sum.py
+++ arrays/1. Two Sum.py
@@ -1,43 +1,33 @@
-"""
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
-Example 1:
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-Example 2:
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-
-Example 3:
-Input: nums = [3,3], target = 6
-Output: [0,1]
-"""
-
-#   Brute Force (Basic)
+#   Brute Force
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        for i in range(0, len(nums)):
-            for j in range(i +1, len(nums)):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
 
 
-#   Dictionary (Hash Map)
+#   Using Hash Map
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dictionary = {}
-        for i, num in enumerate(nums):
-            complement = target - num
+        saw = {}
 
-            if complement in dictionary:
-                return [dictionary[complement], i]
-            dictionary[num] = i 
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in saw:
+                return [i, saw[diff]]
+            saw[nums[i]] = i
+
+
+#   Using enumerate() Pythonic way
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        saw = {}
+
+        for index, value in enumerate(nums):
+            if target - value in saw:
+                return [saw[target - value], index]
+            saw[value] = index
