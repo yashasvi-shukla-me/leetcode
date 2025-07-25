bc707dd55d7f6c7bfcac2a7335c2924c62f22796
Yashasvi Shukla <yashasvigot@gmail.com>
2025-07-25 13:55:34 +0530

    4Sum solution

---
+++ arrays/18. 4Sum.py
@@ -0,0 +1,58 @@
+"""
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+Example 2:
+
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+"""
+
+#   Using Two Pointers
+#   Fix nums[i] and nums[j] (two loops)
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        nums.sort()
+        quad = []   # for storing quadruplets
+        n = len(nums)
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue    # Skipping duplicates
+            for j in range(i + 1, n):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue    # Skipping duplicates
+
+                left = j + 1
+                right = n - 1
+
+                while left < right:
+                    curr_sum = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if curr_sum < target:
+                        left = left + 1
+                    elif curr_sum > target:
+                        right = right - 1
+                    else:
+                        quad.append([nums[i], nums[j], nums[left], nums[right]])
+
+                        while left < right and nums[left] == nums[left + 1]:
+                            left = left + 1
+                        
+                        while left < right and nums[right] == nums[right - 1]:
+                            right = right - 1
+
+                        left = left + 1
+                        right = right - 1
+        return quad
