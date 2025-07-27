352573a61c3b522edc726fa8f458c77e5aa338a9
Yashasvi Shukla <yashasvigot@gmail.com>
2025-07-27 13:25:35 +0530

    Subarray Product Less than K solution

---
+++ arrays/713. Subarray Product Less Than K.py
@@ -0,0 +1,41 @@
+"""
+Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+Example 1:
+
+Input: nums = [10,5,2,6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are:
+[10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+
+Example 2:
+
+Input: nums = [1,2,3], k = 0
+Output: 0
+"""
+
+#   Here the elements of nums are postive and >= 1, so no problem of product 0 or handling (-)ves
+
+#   Using Sliding Window
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+
+        if k <= 1:  # no elements are less than 1
+            return 0
+
+        count = 0
+        left = 0
+        prod = 1
+
+        for right in range(len(nums)):
+            prod = prod * nums[right]
+
+            while prod >= k:
+                prod = prod // nums[left]   # decreasing i.e shrinking the window
+                left = left + 1
+
+            count = count + right - left + 1
+
+        return count
