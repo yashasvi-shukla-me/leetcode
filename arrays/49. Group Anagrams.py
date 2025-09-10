3b473c8904617d9e37bacd09a621fa6a53870e83
Yashasvi Shukla <yashasvigot@gmail.com>
2025-09-10 13:15:56 +0530

    Group Anagrams Solution

---
+++ arrays/49. Group Anagrams.py
@@ -0,0 +1,59 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+"""
+
+#   Using Sorting - HashMap
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = "".join(sorted(word))
+            groups[key].append(word) 
+
+        return list(groups.values())
+
+
+#   Using Count - HashMap
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        groups = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
+            key = tuple(count)
+
+            groups[key].append(word)
+
+        return list(groups.values())
