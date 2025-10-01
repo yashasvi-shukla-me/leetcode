367332f1379c1b0865f8a88775e7d7de02dcff41
Yashasvi Shukla <yashasvigot@gmail.com>
2025-10-01 12:57:40 +0530

    Updated solution

--- arrays/78. Subsets.py
+++ arrays/78. Subsets.py
@@ -14,6 +14,8 @@ Input: nums = [0]
 Output: [[],[0]]
 """
 
+#   Brute Force using Bitmasking
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
 
@@ -28,4 +30,28 @@ class Solution:
                     subset.append(nums[j])
             res.append(subset)
         
+        return res
+    
+    
+#   Using Backtracking
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        res = []
+        subset = []
+
+        def backtrack(i):
+
+            if i >= len(nums):
+                res.append(subset[:])
+                return
+            
+            subset.append(nums[i])
+            backtrack(i + 1)
+            subset.pop()
+
+            backtrack( i + 1)
+
+        backtrack(0)
         return res
