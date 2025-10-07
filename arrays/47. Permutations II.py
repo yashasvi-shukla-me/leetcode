656b15b623c6a026765cfa9390c2025ef85aa6a7
Yashasvi Shukla <yashasvigot@gmail.com>
2025-10-07 10:44:07 +0530

    Solution for Permutations II

---
+++ arrays/47. Permutations II.py
@@ -0,0 +1,48 @@
+"""
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+
+Example 2:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+"""
+
+#   Backtracking approach
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        res = []
+        path = []
+        used = [False] * len(nums)
+        nums.sort()
+
+        def backtrack():
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+                backtrack()
+
+                path.pop()
+                used[i] = False
+
+        backtrack()
+        return res
