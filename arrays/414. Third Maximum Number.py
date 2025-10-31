917c6647e7703c0e17e0ec068dd990c69ef2e388
Yashasvi Shukla <yashasvigot@gmail.com>
2025-10-31 13:34:11 +0530

    added solution for LeetCode 414 Third Maximum Number

--- arrays/414. Third Maximum Number.py
+++ arrays/414. Third Maximum Number.py
@@ -29,35 +29,39 @@ The second distinct maximum is 2 (both 2's are counted together since they have 
 The third distinct maximum is 1.
 """
 
-#   Using Set
+#   Using Sorting
+#   O(nlogn) due to sorting
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        
+        nums = list(set(nums))
+        nums.sort(reverse=True)
+
+        if len(nums) < 3:
+            return max(nums)
+
+        return nums[2]
+
+
+#   K Tracker's Technique
+#   O(n) time and O(1) space
 
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
 
-        unique = sorted(set(nums), reverse=True)
-
-        if len(unique) >= 3:
-            return unique[2]
-        return unique[0]
-
-
-#   Optimal using Three Trackers 
-#   It's In-Place and takes O(1) space
-
-class Solution:
-    def thirdMax(self, nums: List[int]) -> int:
         first = second = third = float('-inf')
 
         for num in nums:
-            if num in (first, second, third):
-                continue    # skip if it exists
+
+            if num == first or num == second or num == third:
+                continue
 
             if num > first:
-                third, second, first = second, first, num   # just swapping
+                first, second, third = num, first, second
             elif num > second:
-                third, second = second, num
+                second, third = num, second
             elif num > third:
                 third = num
 
         return third if third != float('-inf') else first
-        #   in this we check if the len(nums) <= 2, if yes then we return the max i.e first
