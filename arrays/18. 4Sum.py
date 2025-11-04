b6081269defbfe7e5f120c55217d11ef4bd6f1c2
Yashasvi Shukla <yashasvigot@gmail.com>
2025-11-04 19:01:26 +0530

    added solution for LeetCode 18 4Sum

--- arrays/18. 4Sum.py
+++ arrays/18. 4Sum.py
@@ -17,43 +17,45 @@ Input: nums = [2,2,2,2,2], target = 8
 Output: [[2,2,2,2]]
 """
 
-#   Using Two Pointers
+#   Two Pointers
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        
-        quad = []
+
         nums.sort()
         n = len(nums)
+        result = []
 
-        # i and j are the two numbers of quadruplets
-        # left and right are the other two
-
-        for i in range(n - 3):
+        for i in range(n):
             if i > 0 and nums[i] == nums[i - 1]:
-                continue # skipping duplicates
-            
-            for j in range(i + 1, n - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue # skipping duplicates
+                continue
 
-                left, right = j + 1, n - 1
+            for j in range(i + 1, n):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = n - 1
 
                 while left < right:
 
                     total = nums[i] + nums[j] + nums[left] + nums[right]
 
                     if total == target:
-                        quad.append([nums[i], nums[j], nums[left], nums[right]])
-                        left = left + 1
-                        right = right - 1
+                        result.append([nums[i] , nums[j], nums[left] , nums[right]])
 
-                        while left < right and nums[left] == nums[left - 1]:
-                            left = left + 1 # checking until diff value for left
-                        while left < right and nums[right] == nums[right + 1]:
-                            right = right - 1 # checking until diff value for right
+                        while left < right and nums[left] == nums[left + 1]:
+                            left = left + 1
+
+                        while left < right and nums[right] == nums[right - 1]:
+                            right = right - 1
+
+                        left += 1
+                        right -= 1
+
                     elif total < target:
                         left = left + 1
                     else:
                         right = right - 1
-        return quad
+
+        return result
