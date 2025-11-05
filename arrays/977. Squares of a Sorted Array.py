c03bd0d017086f74c458f26b6e9630716e74fb49
Yashasvi Shukla <yashasvigot@gmail.com>
2025-11-05 12:56:40 +0530

    added solution for LeetCode 977 Squares of a Sorted Array

--- arrays/977. Squares of a Sorted Array.py
+++ arrays/977. Squares of a Sorted Array.py
@@ -14,35 +14,36 @@ Input: nums = [-7,-3,2,3,11]
 Output: [4,9,9,49,121]
 """
 
-#   Brute Force Approach
-
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        result = sorted([x*x for x in nums])
-        return result
-
-
-#   Using Two Pointers 
+#   Brute Force
 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
 
-        # array is sorted which helps
-        # pos is for putting values in squares array
-        
+        ls = [x * x for x in nums]
+        ls.sort()
+
+        return ls
+
+
+#   Two Pointers
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+
         n = len(nums)
+        squared = [0] * n
+        pos = right = n - 1
         left = 0
-        right = n - 1
-        pos = n - 1
-        squares = [0] * n # array of zeroes
 
         while left <= right:
 
-            if abs(nums[left]) > abs(nums[right]): # simple if u think about it
-                squares[pos] = nums[left] ** 2
-                left = left + 1
-            else:
-                squares[pos] = nums[right] ** 2
+            if abs(nums[left]) < abs(nums[right]):
+                squared[pos] = nums[right] ** 2
                 right = right - 1
+            else:
+                squared[pos] = nums[left] ** 2
+                left = left + 1
+
             pos = pos - 1
-        return squares
+            
+        return squared
