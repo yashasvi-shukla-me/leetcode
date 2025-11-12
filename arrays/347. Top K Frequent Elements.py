58645d382c0ae38cfde80274c2228c5a0b754776
Yashasvi Shukla <yashasvigot@gmail.com>
2025-11-12 16:35:24 +0530

    added solution for LeetCode 347 Top K Frequent Elements

--- arrays/347. Top K Frequent Elements.py
+++ arrays/347. Top K Frequent Elements.py
@@ -17,42 +17,38 @@ Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
 Output: [1,2]
 """
 
+#   Brute Force Approach
+#   Using Counter + Sorting
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        freq = Counter(nums)
+
+        sortedElements = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+        topK = [element for element, count in sortedElements[:k]]
+
+        return topK
+
+
 #   Using Heap
+#   Time Complexity: O(N log K), Space Complexity: O(N)
 
 import heapq
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
         freq = Counter(nums)
-        heap = []   # initalized an empty heap
-
+        minHeap = []
 
         for num, count in freq.items():
-            heapq.heappush(heap, (count, num)) # pushes into heap (count, nums) 
-            # smallest count at root
+            heapq.heappush(minHeap, (count, num))
 
-            if len(heap) > k:
-                heapq.heappop(heap) # root is popped
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
 
-        return [num for count, num in heap] # out of count, num we want num
-
-
+        return [num for count, num in minHeap]
+    
 #   Using Bucket Sort
-
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
-        freq = Counter(nums)
-
-        bucket = [[] for x in range(len(nums)+1)]
-        for num, count in freq.items():
-
-            bucket[count].append(num)
-
-        result = []
-        for i in range(len(bucket) - 1, 0, -1):
-            for num in bucket[i]:
-                result.append(num)
-                if len(result) == k:
-                    return result
+#   Time Complexity: O(N), Space Complexity: O(N)
