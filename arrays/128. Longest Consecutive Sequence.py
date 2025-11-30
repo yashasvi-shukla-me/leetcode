cb309bd3edb37e05f373b2a1b015f87388aac844
Yashasvi Shukla <yashasvigot@gmail.com>
2025-11-30 11:25:52 +0530

    added solution for LeetCode 128 Longest Consecutive Sequence

--- arrays/128. Longest Consecutive Sequence.py
+++ arrays/128. Longest Consecutive Sequence.py
@@ -19,44 +19,25 @@ Input: nums = [1,0,1,2]
 Output: 3
 """
 
-#   Brute Force using Sorting
-
-class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-
-        if not nums:
-            return 0
-
-        longest = 1
-        curr_len = 1
-        nums.sort()
-
-        for i in range(1, len(nums)):
-
-            if nums[i] == nums[i-1] + 1:
-                curr_len = curr_len + 1
-            elif nums[i] != nums[i - 1]:
-                curr_len = 1
-            longest = max(longest, curr_len)
-
-        return longest
-
-
 #   Using HashSet
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
 
-        hashSet = set(nums)
+        numSet = set(nums)
         longest = 0
 
-        for num in hashSet:
-            if num - 1 not in hashSet:
+        for num in numSet:
+
+            if (num - 1) not in numSet:
+
                 length = 1
-                curr = num
-                while curr + 1 in hashSet:
-                    length = length + 1
-                    curr = curr + 1
+                current = num
+
+                while (current + 1) in numSet:
+                    length += 1
+                    current += 1
+
                 longest = max(longest, length)
 
         return longest
