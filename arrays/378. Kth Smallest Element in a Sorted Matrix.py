4257f1047fb5c315676da7c3ad42b0abf9829f54
Yashasvi Shukla <yashasvigot@gmail.com>
2026-01-25 22:51:39 +0530

    Kth Smallest Element in a Sorted Matrix solution

--- arrays/378. Kth Smallest Element in a Sorted Matrix.py
+++ arrays/378. Kth Smallest Element in a Sorted Matrix.py
@@ -17,24 +17,75 @@ Input: matrix = [[-5]], k = 1
 Output: -5
 """
 
-#   Using Heap
-
-import heapq
+# Using Heaps
+# but it is not optimal
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
 
-        heap = []
+        heap = [] # heap is a list
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(n):
+                heapq.heappush(heap, matrix[i][j])
+
+        for _ in range(k-1):
+            heapq.heappop(heap)
+
+        return heap[0]
+
+
+# Improved Heap, still not Optimal
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+
+        heap = [] # heap is a list
         n = len(matrix)
 
         for r in range(n):
             heapq.heappush(heap, (matrix[r][0], r, 0))
 
-        val = -1
-        for i in range(k):
+        for _ in range(k):
             val, r, c = heapq.heappop(heap)
 
             if c + 1 < n:
                 heapq.heappush(heap, (matrix[r][c + 1], r, c + 1))
-        
+
         return val
+
+
+# Optimal method
+# Using Binary Search
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+
+        n = len(matrix)
+        low = matrix[0][0]
+        high = matrix[n - 1][n - 1]
+
+        def count_less_equal(mid):
+            count = 0
+            row = 0
+            col = n - 1
+
+            while row < n and col >= 0:
+                if matrix[row][col] <= mid:
+                    count = count + col + 1
+                    row = row + 1
+                else:
+                    col = col - 1
+
+            return count
+
+        while low < high:
+            mid = (low + high) // 2
+
+            if count_less_equal(mid) < k:
+                low = mid + 1
+            else:
+                high = mid 
+
+        return low # or high
