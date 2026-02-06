fd743a88773edd851a34ba2c2cb07a175cdfd4d6
Yashasvi Shukla <yashasvigot@gmail.com>
2026-02-06 10:36:38 +0530

    Implement Queue using Stacks solution

--- stack_queue/232. Implement Queue using Stacks.py
+++ stack_queue/232. Implement Queue using Stacks.py
@@ -29,26 +29,27 @@ myQueue.pop(); // return 1, queue is [2]
 myQueue.empty(); // return false
 """
 
+# Using two stacks
+
 class MyQueue:
 
     def __init__(self):
         self.s1 = []
         self.s2 = []
-
+        
     def push(self, x: int) -> None:
         self.s1.append(x)
 
     def pop(self) -> int:
-        if not self.s2:
-            while self.s1:
-                self.s2.append(self.s1.pop())
+        self.peek()
         return self.s2.pop()
-
+        
     def peek(self) -> int:
         if not self.s2:
             while self.s1:
                 self.s2.append(self.s1.pop())
         return self.s2[-1]
+        
 
     def empty(self) -> bool:
         return not self.s1 and not self.s2
